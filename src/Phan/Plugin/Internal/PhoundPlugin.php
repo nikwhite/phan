e001859f9198be5d3767c9244fac7cfa35628797
Nik White <nwhite@etsy.com>
2025-05-12 19:01:31 -0400

    add class, interface, and trait hierarchies to phound

--- src/Phan/Plugin/Internal/PhoundPlugin.php
+++ src/Phan/Plugin/Internal/PhoundPlugin.php
@@ -5,15 +5,19 @@ declare(strict_types=1);
 use ast\Node;
 use Phan\AST\ContextNode;
 use Phan\Language\Context;
+use Phan\Language\Element\Clazz;
 use Phan\PluginV3\PluginAwarePostAnalysisVisitor;
 use Phan\CodeBase;
 use Phan\Language\Element\ClassElement;
 use Phan\Config;
 
 /**
- * Populates a sqlite database with callsites of class elements. Class elements include
- * methods, static methods, properties, static properties, and constants. The database
- * can be queried to find callsites of a given class element.
+ * Populates a sqlite database with callsites of class elements, as well as class, trait, and interface
+ * hierarchies. Class elements include methods, static methods, properties, static properties,
+ * and constants. Class heirarchies include classes and their parent-child relationships, interfaces, and traits.
+ * 
+ * The database can be queried to find callsites of a given class element as well as class, trait,
+ * and interface hierarchy. 
  *
  * Examples:
  *
@@ -28,24 +32,135 @@ use Phan\Config;
  *
  * 4) Search for callsites of the \Foo::BANG constant:
  *     select * from callsites where element = '\Foo::BANG' and type = 'const' order by callsite
+ * 
+ * 5) Search for all classes which extend from a base class:
+ * 
+ * 6) Find all concrete classes using a particular trait:
+ * 
+ * 7) Search for all classes which implement a given interface:
+ * 
+ * 8) Search for 
+ * 
  */
 final class PhoundVisitor extends PluginAwarePostAnalysisVisitor
 {
 
-    private const NUM_DB_COLS = 3; // element, type, callsite
+    private const MAX_COLS = 3; // table with the most columns (callsites): element, type, callsite
 
     // Avoid `SQLite3::prepare(): Unable to prepare statement: 1, too many SQL variables`
     // See #9: https://www.sqlite.org/limits.html
-    private const BULK_INSERT_SIZE = 999 / self::NUM_DB_COLS;
+    private const BULK_INSERT_SIZE = intval(999 / self::MAX_COLS);
 
     /** @var SQLite3 */
     private static $db;
-
-    /** @var SQLite3Stmt */
-    private static $prepared_insert;
+    private static SQLite3Stmt $callsites_prepared_insert;
+    private static SQLite3Stmt $classes_prepared_insert;
+    private static SQLite3Stmt $interfaces_prepared_insert;
+    private static SQLite3Stmt $traits_prepared_insert;
+    private static SQLite3Stmt $interface_relationships_prepared_insert;
+    private static SQLite3Stmt $interface_interfaces_prepared_insert;
+    private static SQLite3Stmt $class_relationships_prepared_insert;
 
     /** @var list<array{string,string,string}> */
     private static $callsites = [];
+    private static $classes = [];
+    private static $interfaces = [];
+    private static $traits = [];
+    private static $trait_traits = [];
+    private static $interface_relationships = [];
+    private static $interface_interfaces = [];
+    private static $class_relationships = [];
+    private static $class_interfaces = [];
+    private static $class_traits = [];
+
+    private const TABLES = [
+        'callsites' => [
+            'columns' => [
+                'element TEXT NOT NULL',
+                'type TEXT NOT NULL',
+                'callsite TEXT NOT NULL',
+                'id INTEGER PRIMARY KEY',
+            ]
+        ],
+        'classes' => [
+            'columns' => [
+                'name TEXT NOT NULL PRIMARY KEY',
+                'filepath TEXT NOT NULL',
+            ]
+        ],
+        'interfaces' => [
+            'columns' => [
+                'name TEXT NOT NULL PRIMARY KEY',
+                'filepath TEXT NOT NULL',
+            ]
+        ],
+        'traits' => [
+            'columns' => [
+                'name TEXT NOT NULL PRIMARY KEY',
+                'filepath TEXT NOT NULL',
+            ]
+        ],
+        'trait_traits' => [
+            'columns' => [
+                'trait TEXT',
+                'uses_trait TEXT',
+            ],
+            'relationships' => [
+                'FOREIGN KEY(trait) REFERENCES traits(name)',
+                'FOREIGN KEY(uses_trait) REFERENCES traits(name)',
+            ]
+        ],
+        'interface_relationships' => [
+            'columns' => [
+                'parent TEXT',
+                'child TEXT',
+            ],
+            'relationships' => [
+                'FOREIGN KEY(parent) REFERENCES interfaces(name)',
+                'FOREIGN KEY(child) REFERENCES interfaces(name)',
+            ]
+        ],
+        'interface_interfaces' => [
+            'columns' => [
+                'interface TEXT',
+                'implements TEXT',
+            ],
+            'relationships' => [
+                'FOREIGN KEY(interface) REFERENCES interfaces(name)',
+                'FOREIGN KEY(implements) REFERENCES interfaces(name)',
+            ]
+        ],
+        'class_relationships' => [
+            'columns' => [
+                'parent TEXT',
+                'child TEXT',
+            ],
+            'relationships' => [
+                'FOREIGN KEY(parent) REFERENCES classes(name)',
+                'FOREIGN KEY(child) REFERENCES classes(name)',
+            ]
+        ],
+        'class_interfaces' => [
+            'columns' => [
+                'class TEXT',
+                'interface TEXT',
+            ],
+            'relationships' => [
+                'FOREIGN KEY(class) REFERENCES classes(name)',
+                'FOREIGN KEY(interface) REFERENCES interfaces(name)',
+            ]
+        ],
+        'class_traits' => [
+            'columns' => [
+                'class TEXT',
+                'trait TEXT',
+            ],
+            'relationships' => [
+                'FOREIGN KEY(trait) REFERENCES classes(name)',
+                'FOREIGN KEY(trait) REFERENCES traits(name)',
+            ]
+        ],
+    ];
 
     /**
      * @param CodeBase $code_base
@@ -65,21 +180,24 @@ final class PhoundVisitor extends PluginAwarePostAnalysisVisitor
         }
         self::$db = new SQLite3($db_path);
 
-        if (!self::$db->exec('DROP TABLE IF EXISTS callsites')) {
-            throw new Exception();
+        // cleanup in reverse for foreign key constraints
+        foreach (array_reverse(self::TABLES) as $table => $table_meta) {
+            if (!self::$db->exec("DROP TABLE IF EXISTS $table")) {
+                throw new Exception();
+            }
         }
 
-        if (!self::$db->exec(
-            <<<'EOD'
-            create table callsites(
-                element TEXT NOT NULL,
-                type TEXT NOT NULL,
-                callsite TEXT NOT NULL,
-                PRIMARY KEY (element, type, callsite)
-            )
-EOD
-        )) {
-            throw new Exception();
+        // build tables in natural order
+        foreach (self::TABLES as $table => $table_meta) {
+            $column_str = implode(', ', $table_meta['columns']);
+
+            if (isset($table['relationships'])) {
+                $column_str .= ', ' . implode(', ', $table_meta['relationships']);
+            }
+
+            if (!self::$db->exec("create table $table($column_str)")) {
+                throw new Exception();
+            }
         }
 
         if (!self::$db->exec('CREATE INDEX element_and_callsite ON callsites (element, callsite)')) {
@@ -92,11 +210,20 @@ EOD
         if (!self::$db->exec("PRAGMA journal_mode = OFF")) {
             throw new Exception();
         }
+        if (!self::$db->exec("PRAGMA foreign_keys = ON")) {
+            throw new Exception();
+        }
         if (!self::$db->exec("PRAGMA page_size = 4096")) {
             throw new Exception();
         }
 
-        self::$prepared_insert = $this->createBulkInsertPreparedStatement(self::BULK_INSERT_SIZE);
+        self::$callsites_prepared_insert  = $this->createBulkInsertPreparedStatement(self::BULK_INSERT_SIZE);
+        self::$classes_prepared_insert    = $this->createBaseNodeBulkInsertPreparedStatement("classes", self::BULK_INSERT_SIZE);
+        self::$interfaces_prepared_insert = $this->createBaseNodeBulkInsertPreparedStatement("interfaces", self::BULK_INSERT_SIZE);
+        self::$traits_prepared_insert     = $this->createBaseNodeBulkInsertPreparedStatement("traits", self::BULK_INSERT_SIZE);
+        self::$interface_relationships_prepared_insert = $this->createRelationshipBulkInsertPreparedStatement("interface_relationships", self::BULK_INSERT_SIZE);
+        self::$interface_interfaces_prepared_insert    = $this->createRelationshipBulkInsertPreparedStatement("interface_interfaces", self::BULK_INSERT_SIZE);
+        self::$class_relationships_prepared_insert     = $this->createRelationshipBulkInsertPreparedStatement("class_relationships", self::BULK_INSERT_SIZE);
     }
 
     /**
@@ -109,6 +236,41 @@ EOD
         $bulk_insert_sql = rtrim($bulk_insert_sql, ', ');
         $stmt = self::$db->prepare($bulk_insert_sql);
         if ($stmt === false) {
+            throw new Exception();
+        }
+        return $stmt;
+    }
+
+    /**
+     * @param string    $table_name
+     */
+    private static function createBaseNodeBulkInsertPreparedStatement(
+        string $table_name,
+        int $bulk_insert_size
+    ): SQLite3Stmt {
+        $bulk_insert_sql = "INSERT INTO $table_name ( 'name', 'filepath') VALUES ";
+        $bulk_insert_sql .= str_repeat("(?, ?), ", $bulk_insert_size);
+        $bulk_insert_sql = rtrim($bulk_insert_sql, ', ');
+        if (!$stmt = self::$db->prepare($bulk_insert_sql)) {
+            throw new Exception();
+        }
+        return $stmt;
+    }
+
+    private static function createRelationshipBulkInsertPreparedStatement(
+        string $table_name,
+        int $bulk_insert_size
+    ): SQLite3Stmt {
+        $table_meta = self::TABLES[$table_name];
+        $col_names = [];
+        foreach ($table_meta['columns'] as $column) {
+            $col_names[] = "'" . explode(' ', $column)[0] . "'";
+        }
+        $insert_columns = str_replace(' TEXT', '', implode(', ', $col_names));
+        $bulk_insert_sql = "INSERT INTO $table_name ($insert_columns) VALUES ";
+        $bulk_insert_sql .= str_repeat("(?, ?), ", $bulk_insert_size);
+        $bulk_insert_sql = rtrim($bulk_insert_sql, ', ');
+        if (!$stmt = self::$db->prepare($bulk_insert_sql)) {
             throw new Exception();
         }
         return $stmt;
@@ -248,7 +410,7 @@ EOD
             self::$callsites[] = [$element_name, $type, $callsite];
 
             if (count(self::$callsites) >= self::BULK_INSERT_SIZE) {
-                self::doBulkWrite(self::$prepared_insert);
+                self::doBulkWrite(self::$callsites_prepared_insert);
             }
         }
     }
@@ -268,20 +430,173 @@ EOD
             $stmt->bindValue($bind_index, $callsite[2], SQLITE3_TEXT);
             $bind_index++;
         }
+        self::execStatement($stmt);
+        self::$callsites = [];
+    }
 
+    /**
+     * Called when visiting classes, interfaces, and traits, including anonymous 
+     * versions of the same. Phan generates FQSENs consistently for anonymous classes 
+     * using file modification time, i.e. if a file contains anonymous_class_83cba571, 
+     * it will always contain anonymous_class_83cba571 unless/until that file is modified.
+     */
+    public function visitClass(Node $node) {
+        if (!$this->context->isInClassScope()) {
+            return;
+        }
+
+        $clazz = $this->context->getClassInScope($this->code_base);
+        $filepath = $this->context->getProjectRelativePath();
+
+        if ($clazz->isClass()) {
+            self::handleClass($clazz, $filepath);
+
+        } else if ($clazz->isInterface()) {
+            self::handleInterface($clazz, $filepath);
+
+        } else if ($clazz->isTrait()) {
+            self::handleTrait($clazz, $filepath);
+
+        } else {
+            throw new Exception("Unknown class type: $clazz");
+        }
+    }
+
+    /** 
+     * Processes a class to obtain its name, filepath, relationships, interfaces, and traits.
+     */
+    private static function handleClass(Clazz $clazz, string $filepath) {
+        $name = $clazz->getFQSEN()->__toString();
+        self::$classes[] = [$name, $filepath];
+
+        if (count(self::$classes) >= self::BULK_INSERT_SIZE) {
+            self::doHierarchyBulkWrite(self::$classes, self::$classes_prepared_insert);
+            self::$classes = [];
+        }
+
+        if ($clazz->hasParentType()) {
+            $parent_name = $clazz->getParentClassFQSEN();
+            self::$class_relationships[] = [$parent_name, $name];
+        }
+
+        if (count(self::$class_relationships) >= self::BULK_INSERT_SIZE) {
+            self::doHierarchyBulkWrite(self::$class_relationships, self::$class_relationships_prepared_insert);
+            self::$class_relationships = [];
+        }
+
+        $impl_interfaces = $clazz->getInterfaceFQSENList();
+        foreach ($impl_interfaces as $iface) {
+            self::$class_interfaces[] = [$name, $iface];
+        }
+
+        if (count(self::$class_interfaces) >= self::BULK_INSERT_SIZE) {
+            $stmt = self::createRelationshipBulkInsertPreparedStatement("class_interfaces", count(self::$class_interfaces));
+            self::doHierarchyBulkWrite(self::$class_interfaces, $stmt);
+            self::$class_interfaces = [];
+        }
+
+        $uses_traits = $clazz->getTraitFQSENList();
+        foreach ($uses_traits as $trait) {
+            self::$class_traits[] = [$name, $trait];
+        }
+
+        if (count(self::$class_traits) >= self::BULK_INSERT_SIZE) {
+            $stmt = self::createRelationshipBulkInsertPreparedStatement("class_traits", count(self::$class_traits));
+            self::doHierarchyBulkWrite(self::$class_traits, $stmt);
+            self::$class_traits = [];
+        }
+    }
+
+    /** 
+     * Processes an interface to obtain its name, filepath, relationships, and implemented interfaces.
+     */
+    private static function handleInterface(Clazz $clazz, string $filepath) {
+        $name = $clazz->getFQSEN()->__toString();
+        self::$interfaces[] = [$name, $filepath];
+
+        if (count(self::$interfaces) >= self::BULK_INSERT_SIZE) {
+            self::doHierarchyBulkWrite(self::$interfaces, self::$interfaces_prepared_insert);
+            self::$interfaces = [];
+        }
+
+        if ($clazz->hasParentType()) {
+            $parent_name = $clazz->getParentClassFQSEN();
+            self::$interface_relationships[] = [$parent_name, $name];
+        }
+
+        if (count(self::$interface_relationships) >= self::BULK_INSERT_SIZE) {
+            self::doHierarchyBulkWrite(
+                self::$interface_relationships,
+                self::$interface_relationships_prepared_insert
+            );
+            self::$interface_relationships = [];
+        }
+
+        if (count($clazz->getInterfaceFQSENList()) > 0) {
+            $impl_interfaces = $clazz->getInterfaceFQSENList();
+            foreach ($impl_interfaces as $iface) {
+                self::$interface_interfaces[] = [$name, $iface];
+            }
+        }
+        
+        if (count(self::$interface_interfaces) >= self::BULK_INSERT_SIZE) {
+            self::doHierarchyBulkWrite(
+                self::$interface_interfaces,
+                self::$interface_interfaces_prepared_insert
+            );
+            self::$interface_interfaces = [];
+        }
+    }
+
+    /** 
+     * Processes a trait to obtain its name, filepath, and used traits.
+     */
+    private static function handleTrait(Clazz $clazz, string $filepath) {
+        $name = $clazz->getFQSEN()->__toString();
+        self::$traits[] = [$name, $filepath];
+
+        if (count(self::$traits) >= self::BULK_INSERT_SIZE) {
+            self::doHierarchyBulkWrite(self::$traits, self::$traits_prepared_insert);
+            self::$traits = [];
+        }
+
+        $uses_traits = $clazz->getTraitFQSENList();
+        foreach ($uses_traits as $trait) {
+            self::$trait_traits[] = [$name, $trait];
+        }
+
+        if (count(self::$trait_traits) >= self::BULK_INSERT_SIZE) {
+            $stmt = self::createRelationshipBulkInsertPreparedStatement("trait_traits", count(self::$trait_traits));
+            self::doHierarchyBulkWrite(self::$trait_traits, $stmt);
+            self::$trait_traits = [];
+        }
+    }
+
+    /**
+     * Bind any 2 values to a row for both tables and relationship tables, just because they have the same
+     * # of columns. if base tables diverge in # of columns from relationship tables, this breaks.
+     */
+    private static function doHierarchyBulkWrite(array $nodes, SQLite3Stmt $stmt): void {
+        $bind_index = 1;
+        foreach ($nodes as $node) {
+            $stmt->bindValue($bind_index, $node[0], SQLITE3_TEXT);
+            $bind_index++;
+            $stmt->bindValue($bind_index, $node[1], SQLITE3_TEXT);
+            $bind_index++;
+        }
+        self::execStatement($stmt);
+    }
+
+    private static function execStatement(SQLite3Stmt $stmt) {
         if (!$stmt->execute()) {
             throw new Exception();
         }
-
         if (!$stmt->reset()) {
             throw new Exception();
         }
-
         if (!$stmt->clear()) {
             throw new Exception();
         }
-
-        self::$callsites = [];
     }
 
     /**
@@ -289,12 +604,43 @@ EOD
      * @throws Exception
      */
     public static function finalizeProcess(): void {
-        if (count(self::$callsites) <= 0) {
-            return;
+        if (count(self::$callsites) >= 0) {
+            $stmt = self::createBulkInsertPreparedStatement(count(self::$callsites));
+            self::doBulkWrite($stmt);
         }
 
-        $stmt = self::createBulkInsertPreparedStatement(count(self::$callsites));
-        self::doBulkWrite($stmt);
+        if (count(self::$classes) > 0) {
+            $stmt = self::createBaseNodeBulkInsertPreparedStatement("classes", count(self::$classes));
+            self::doHierarchyBulkWrite(self::$classes, $stmt);
+        }
+        if (count(self::$interfaces) > 0) {
+            $stmt = self::createBaseNodeBulkInsertPreparedStatement("interfaces", count(self::$interfaces));
+            self::doHierarchyBulkWrite(self::$interfaces, $stmt);
+        }
+        if (count(self::$traits) > 0) {
+            $stmt = self::createBaseNodeBulkInsertPreparedStatement("traits", count(self::$traits));
+            self::doHierarchyBulkWrite(self::$traits, $stmt);
+        }
+        if (count(self::$class_relationships) > 0) {
+            $stmt = self::createRelationshipBulkInsertPreparedStatement("class_relationships", count(self::$class_relationships));
+            self::doHierarchyBulkWrite(self::$class_relationships, $stmt);
+        }
+        if (count(self::$class_interfaces) > 0) {
+            $stmt = self::createRelationshipBulkInsertPreparedStatement("class_interfaces", count(self::$class_interfaces));
+            self::doHierarchyBulkWrite(self::$class_interfaces, $stmt);
+        }
+        if (count(self::$class_traits) > 0) {
+            $stmt = self::createRelationshipBulkInsertPreparedStatement("class_traits", count(self::$class_traits));
+            self::doHierarchyBulkWrite(self::$class_traits, $stmt);
+        }
+        if (count(self::$interface_relationships) > 0) {
+            $stmt = self::createRelationshipBulkInsertPreparedStatement("interface_relationships", count(self::$interface_relationships));
+            self::doHierarchyBulkWrite(self::$interface_relationships, $stmt);
+        }
+        if (count(self::$trait_traits) > 0) {
+            $stmt = self::createRelationshipBulkInsertPreparedStatement("trait_traits", count(self::$trait_traits));
+            self::doHierarchyBulkWrite(self::$trait_traits, $stmt);
+        }
     }
 
 }
